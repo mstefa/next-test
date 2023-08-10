0477146d08f389ee0c50c829e21babf61598abc9
mstefa <mstefanutti24@gmail.com>
2023-08-10 15:27:28 +0200

    feat/deploy-fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  basePath: 'https://mstefa.github.io/next-test/',
-  images: { unoptimized: true }, // not compatible with output: export
+  // images: { unoptimized: true }, // not compatible with output: export
   experimental: {
     mdxRs: true,
   },
@@ -20,3 +19,4 @@ const withMDX = require('@next/mdx')()
 module.exports = withMDX(nextConfig)
 
 
+// amp, analyticsId, assetPrefix, basePath, cleanDistDir, compiler, compress, configOrigin, crossOrigin, devIndicators, distDir, env, eslint, excludeDefaultMomentLocales, experimental, exportPathMap, generateBuildId, generateEtags, headers, httpAgentOptions, i18n, images, modularizeImports, onDemandEntries, optimizeFonts, output, outputFileTracing, pageExtensions, poweredByHeader, productionBrowserSourceMaps, publicRuntimeConfig, reactStrictMode, redirects, rewrites, sassOptions, serverRuntimeConfig, skipMiddlewareUrlNormalize, skipTrailingSlashRedirect, staticPageGenerationTimeout, swcMinify, target, trailingSlash, transpilePackages, typescript, useFileSystemPublicRoutes, webpack
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,14 +9,14 @@ export default function About() {
       <SectionTitle>About Me</SectionTitle>
       <div >
         <div className={styles.textContainer}>
-          <p>
-            I am an experienced Software Engineer, adept at creating exceptional platforms that offer exceptional user experiences. My expertise lies in backend development, where I've utilized languages such as <b>TypeScript</b>, <b>GoLang</b>, and <b>Java </b> to architect innovative microservices. Additionally, I possess frontend proficiency with <b>React</b>. I'm motivated for  elevate platform quality and performance while cultivating robust customer relationships through cutting-edge solutions.
+        <p>
+            I am an experienced Software Engineer, adept at creating exceptional platforms that offer exceptional user experiences. My expertise lies in backend development, where I&apos;ve utilized languages such as <b>TypeScript</b>, <b>GoLang</b>, and <b>Java </b> to architect innovative microservices. Additionally, I possess frontend proficiency with <b>React</b>. I&apos;m motivated for  elevate platform quality and performance while cultivating robust customer relationships through cutting-edge solutions.
           </p>
           <p>
             From my root as an aeronautical engineer, my journey has been a transformative one, molding me into a versatile Software Engineer. On my journey, I got the chance to dive into the role of a Teaching Assistant, mentoring emerging talents and guiding into the development world. My focus on effective leadership and ability to adapt to various cultures have consistently led to successful outcomes across a wide range of domains. 
             </p>
           <p>
-            <b><a  href='https://www.linkedin.com/in/matiasstefanutti/' target="blank" >Let's connect </a></b> to explore new horizons and drive transformative tech solutions.
+            <b><a  href='https://www.linkedin.com/in/matiasstefanutti/' target="blank" >Let&apos;s connect </a></b> to explore new horizons and drive transformative tech solutions.
           </p>
 
 
