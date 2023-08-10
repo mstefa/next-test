640dd941376308bd6deb83284ed686fe2072d8cf
mstefa <mstefanutti24@gmail.com>
2023-08-10 15:33:02 +0200

    fix: imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import './globals.css'
-import About from '@/src/components/about/About'
+import About from '../src/components/about/About'
 import Presentation from '../src/components/presentation/Presentation'
-import NavBar from '@/src/components/navBar/NavBar';
-import SocialMedia from '@/src/components/socialmedia/SocialMedia';
-import Jobs from '@/src/components/jobs/Jobs';
-import Projects from '@/src/components/projects/Projects';
-import Contact from '@/src/components/contact/Contact';
-import { Footer } from '@/src/components/footer/Footer';
+import NavBar from '../src/components/navBar/NavBar';
+import SocialMedia from '../src/components/socialmedia/SocialMedia';
+import Jobs from '../src/components/jobs/Jobs';
+import Projects from '../src/components/projects/Projects';
+import Contact from '../src/components/contact/Contact';
+import { Footer } from '../src/components/footer/Footer';
 
 export default function Home() {
   return (
