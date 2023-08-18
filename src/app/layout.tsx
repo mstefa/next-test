d76a8cb5c5eb92eeb7637baaaf6ea2ade42932c7
mstefa <mstefanutti24@gmail.com>
2023-08-18 15:38:57 +0200

    fix: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 export const metadata = {
   title: 'mstefa',
   description: 'Matias Stefanutti personal web site',
+  icons: {
+    icon: './favicon.ico',
+  }
 }
 
 export default function RootLayout({
